f19e7c7c9d74a8e69e45ec3ea25b7ece949a15b1
Ko yunmin <yunmin1992@sogang.ac.kr>
2019-10-18 22:47:25 +0900

    Image 받아오기 및 Image 출력하기 Camera Image 받는 경로 설정 확인할 것

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,16 +1,79 @@
 #include <jni.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+#include <android/log.h>
+#include <android/bitmap.h>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/features2d/features2d.hpp>
+
+#include <algorithm>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+using namespace std;
 using namespace cv;
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_arucodetection_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
-                                                                      jlong matAddrInput,
-                                                                      jlong matAddrResult) {
+                                                              jint src_width, jint src_height,
+                                                              jobject buffer, jobject dst) {
     // TODO: implement ConvertRGBtoGray()
-    Mat &matInput = *(Mat *)matAddrInput;
-    Mat &matResult = *(Mat *)matAddrResult;
+    uint8_t *srcLumaPtr = reinterpret_cast<uint8_t *>(env->GetDirectBufferAddress(buffer));
 
-    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
+    int dstWidth;
+    int dstHeight;
+
+    cv::Mat mYuv(src_height + src_height / 2, src_width, CV_8UC1, srcLumaPtr);
+
+    ANativeWindow *win = ANativeWindow_fromSurface(env, dst);
+    ANativeWindow_acquire(win);
+
+    ANativeWindow_Buffer buf;
+
+    dstWidth = src_height;
+    dstHeight = src_width;
+
+    ANativeWindow_setBuffersGeometry(win, dstWidth, dstHeight, 0 /*format unchanged*/);
+
+    if (int32_t err = ANativeWindow_lock(win, &buf, NULL)) {
+        ANativeWindow_release(win);
+        return;
+    }
+
+    uint8_t *dstLumaPtr = reinterpret_cast<uint8_t *>(buf.bits);
+    Mat dstRgba(dstHeight, buf.stride, CV_8UC4,
+                dstLumaPtr);        // TextureView buffer, use stride as width
+    Mat srcRgba(src_height, src_width, CV_8UC4);
+    Mat flipRgba(dstHeight, dstWidth, CV_8UC4);
+    Mat tempGray(src_height, src_width, CV_8UC4);
+
+    cv::cvtColor(mYuv, srcRgba, COLOR_YUV2RGBA_NV21);
+
+    //cvtColor(tempGray, srcRgba, COLOR_RGBA2GRAY);
+
+    // Rotate 90 degree
+    cv::transpose(srcRgba, flipRgba);
+    cv::flip(flipRgba, flipRgba, 1);
+
+    uchar *dbuf;
+    uchar *sbuf;
+    dbuf = dstRgba.data;
+    sbuf = flipRgba.data;
+    int i;
+    for (i = 0; i < flipRgba.rows; i++) {
+        dbuf = dstRgba.data + i * buf.stride * 4;
+        memcpy(dbuf, sbuf, flipRgba.cols * 4);
+        sbuf += flipRgba.cols * 4;
+    }
+
+    ANativeWindow_unlockAndPost(win);
+    ANativeWindow_release(win);
 }
--- app/src/main/java/com/example/arucodetection/MainActivity.java
+++ app/src/main/java/com/example/arucodetection/MainActivity.java
@@ -1,203 +1,237 @@
 package com.example.arucodetection;
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
 import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.WindowManager;
+import android.widget.Toast;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static android.Manifest.permission.CAMERA;
 
 
-public class MainActivity extends AppCompatActivity
-        implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class MainActivity extends AppCompatActivity{
 
-    private static final String TAG = "opencv";
-    private Mat matInput;
-    private Mat matResult;
-
-    private CameraBridgeViewBase mOpenCvCameraView;
-
-    public native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
-
+    private static final int REQUEST_CAMERA_PERMISSION = 1234;
 
     static {
-        System.loadLibrary("opencv_java4");
         System.loadLibrary("native-lib");
     }
 
 
+    public native void ConvertRGBtoGray(int srcWidth, int srcHeight, ByteBuffer buffer, Surface dst);
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    //public native void setCamera2ImageArUco(Image src, Surface dst);
+
+    private TextureView mTextureView;
+
+    private ImageReader mImageReader;
+
+    private final ImageReader.OnImageAvailableListener mOnImageAvailableListener
+            = new ImageReader.OnImageAvailableListener() {
         @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
+        public void onImageAvailable(ImageReader reader) {
+            Image nextImage = reader.acquireNextImage();
+            Image.Plane[] planes = nextImage.getPlanes();
+            Log.e("SSSSSSSSSSSSSSSSSSSSSSSSS", "planes " + planes.length);
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            texture.setDefaultBufferSize(nextImage.getWidth(), nextImage.getHeight());
+            Surface dstSurface = new Surface(texture);
+            ConvertRGBtoGray(nextImage.getWidth(), nextImage.getHeight(), planes[0].getBuffer(), dstSurface);
+            nextImage.close();
         }
     };
 
+    private CameraDevice mCamera;
+    private Size mPreviewSize;
+    private CameraCaptureSession mCameraSession;
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private HandlerThread mBackgroundThread;
+    private Handler mBackgroundHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
         setContentView(R.layout.activity_main);
 
-        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.activity_surface_view);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
-        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
-    }
-
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-    }
-
-
-    public void onDestroy() {
-        super.onDestroy();
-
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-
-        matInput = inputFrame.rgba();
-
-        if ( matResult == null )
-
-            matResult = new Mat(matInput.rows(), matInput.cols(), matInput.type());
-
-        ConvertRGBtoGray(matInput.getNativeObjAddr(), matResult.getNativeObjAddr());
-
-        return matResult;
-    }
-
-
-    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
-        return Collections.singletonList(mOpenCvCameraView);
-    }
-
-
-    //여기서부턴 퍼미션 관련 메소드
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
-
-
-    protected void onCameraPermissionGranted() {
-        List<? extends CameraBridgeViewBase> cameraViews = getCameraViewList();
-        if (cameraViews == null) {
+        // 카메라 권한 체크
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
             return;
         }
-        for (CameraBridgeViewBase cameraBridgeViewBase: cameraViews) {
-            if (cameraBridgeViewBase != null) {
-                cameraBridgeViewBase.enableView();
+        mBackgroundThread = new HandlerThread("CameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+        Size largest = new Size(5312,2988);
+        mImageReader = ImageReader.newInstance(largest.getWidth(), largest.getHeight(), ImageFormat.JPEG, /*maxImages*/2);
+
+        mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, mBackgroundHandler);
+
+        initTextureView();
+    }
+
+    private void initTextureView() {
+        mTextureView = (TextureView) findViewById(R.id.texture);
+        mTextureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+                Log.e("cklee", "MMM onSurfaceTextureAvailable");
+                openCamera();
             }
-        }
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        boolean havePermission = true;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
-                havePermission = false;
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+                Log.e("cklee", "MMM onSurfaceTextureSizeChanged");
             }
-        }
-        if (havePermission) {
-            onCameraPermissionGranted();
-        }
-    }
 
-    @Override
-    @TargetApi(Build.VERSION_CODES.M)
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            onCameraPermissionGranted();
-        }else{
-            showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                Log.e("cklee", "MMM onSurfaceTextureDestroyed");
+                return false;
+            }
 
-
-    @TargetApi(Build.VERSION_CODES.M)
-    private void showDialogForPermission(String msg) {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this);
-        builder.setTitle("알림");
-        builder.setMessage(msg);
-        builder.setCancelable(false);
-        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id){
-                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+                // 화면 갱신시마다 불림
+//                Log.e("cklee", "MMM onSurfaceTextureUpdated");
             }
         });
-        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
+    }
+
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String[] cameraIdArray = manager.getCameraIdList();
+            Log.e("cklee", "MMM cameraIds = " + Arrays.deepToString(cameraIdArray));
+
+            // test 로 0 번 camera 를 사용
+            String oneCameraId = cameraIdArray[0];
+
+            CameraCharacteristics cameraCharacter = manager.getCameraCharacteristics(oneCameraId);
+            Log.e("cklee", "MMM camraCharacter = " + cameraCharacter);
+
+            StreamConfigurationMap map = cameraCharacter.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            Size[] sizesForStream = map.getOutputSizes(SurfaceTexture.class);
+            Log.e("cklee", "MMM sizesForStream = " + Arrays.deepToString(sizesForStream));
+
+            // 가장 큰 사이즈부터 들어있다
+            mPreviewSize = sizesForStream[0];
+
+            if(ActivityCompat.checkSelfPermission(this, CAMERA) != PackageManager.PERMISSION_GRANTED){
+                return;
+            }
+            manager.openCamera(oneCameraId, new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice cameraDevice) {
+                    mCamera = cameraDevice;
+                    showCameraPreview();
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+                    mCamera.close();
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice cameraDevice, int errorCode) {
+                    Log.e("cklee", "MMM errorCode = " + errorCode);
+                    mCamera.close();
+                    mCamera = null;
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            Log.e("cklee", "MMM openCamera ", e);
+        }
+    }
+
+    private void showCameraPreview() {
+        try {
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            Surface textureViewSurface = new Surface(texture);
+
+            mCaptureRequestBuilder = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            //mCaptureRequestBuilder.addTarget(textureViewSurface);
+            // Caemara Image 경로 변경 2개의 surface 1개는 출력용 1개는 이미지 처리용, 이미지 처리용으로 받아서 opencv로 넘기도록 한다 -> imagereader를 통해
+            mCaptureRequestBuilder.addTarget(mImageReader.getSurface());
+            mCaptureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+            List<Surface> outputSurfaces = new ArrayList<>();
+            outputSurfaces.add(mImageReader.getSurface());
+
+            mCamera.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    mCameraSession = cameraCaptureSession;
+                    updatePreview();
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    Log.e("cklee", "MMM onConfigureFailed");
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            Log.e("cklee", "MMM showCameraPreview ", e);
+        }
+    }
+
+    private void updatePreview() {
+        try {
+            mCameraSession.setRepeatingRequest(mCaptureRequestBuilder.build(), null, null);
+        } catch (CameraAccessException e) {
+            Log.e("cklee", "MMM updatePreview", e);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
                 finish();
             }
-        });
-        builder.create().show();
+        } else {
+            openCamera();
+        }
     }
-
-
 }
